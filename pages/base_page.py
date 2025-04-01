9645465fa9469d0df5569e742583e6d6c993a0db
psPash15 <snopkov.2012@gmail.com>
2025-04-01 17:27:15 +0300

    Initialization LoginPage in the test body.Edited: locators.py, login_page.py, main_page.py, test_main_page.py, base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,14 +5,13 @@ class BasePage():
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
-    
-    def open(self):
-        self.browser.get(self.url)
-        
-    
+     
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
         except (NoSuchElementException):
             return False
         return True
+        
+    def open(self):
+        self.browser.get(self.url)
