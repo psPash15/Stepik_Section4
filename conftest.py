d67046e1dcadac96af52e0befa10265936f013b4
psPash15 <snopkov.2012@gmail.com>
2025-05-13 17:43:45 +0300

    update conftest.py file

--- conftest.py
+++ conftest.py
@@ -6,15 +6,11 @@ def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='en',
                      help="Choose language")
 
-
 @pytest.fixture(scope="function")
 def browser(request):
-
     user_language = request.config.getoption("language")
-    
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    
     print("\nstart chrome browser")
     browser = webdriver.Chrome(options=options)
     yield browser
